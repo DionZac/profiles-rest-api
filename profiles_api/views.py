f56ad6cbebd3935c0e18a80180f392a4ccead395
zacdion <zacdion333@gmail.com>
2020-01-11 20:48:36 +0200

    Added Login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 from profiles_api import serializers,models,permissions
@@ -111,4 +113,8 @@ class HelloApiView(APIView):
         """Handle Delete of an object"""
 
         return Response({'method':'DELETE'})
-        
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle createing user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
