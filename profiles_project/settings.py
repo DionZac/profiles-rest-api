6684f89d6b7f243e5d4e0d3b6d531250210448eb
Zacharopoulos Dionisis <zacharopoulos@flipnode.net>
2020-02-06 22:04:05 +0200

    Ready for deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sgzv5&=_p)h_s8%64h@kaa(w-9=mx7q&cf)0yt5rb#t7pht(rc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+
+STATIC_ROOT = 'static/'
